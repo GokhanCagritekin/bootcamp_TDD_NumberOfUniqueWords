9793920d596b55d2ed6015c3df3c71e78aa75f9f
gokhancagritekin <gokhan.cagritekin@gmail.com>
2020-09-19 00:18:57 +0300

    Gokhan-commit2- get rid of repetition

--- src/main/java/Homework2/NumberOfUniqueWords.java
+++ src/main/java/Homework2/NumberOfUniqueWords.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NumberOfUniqueWords {
 
@@ -19,15 +20,16 @@ public class NumberOfUniqueWords {
     public static int calculateNumberOfUniqueWords(String[] words){
         boolean[] array = new boolean[words.length];
         ArrayList<String> nonUniques = new ArrayList<String>();
+        words = Arrays.stream(words).map(s -> s.toLowerCase()).toArray(String[]::new);
         int j, i = 0;
         int count = 0;
         for (i = 0; i < words.length; i++) {
-            if (!array[i] && !(nonUniques.contains(words[i].toLowerCase()))) {
+            if (!array[i] && !(nonUniques.contains(words[i]))) {
                 count++;
                 for (j = i + 1; j < words.length; j++) {
-                    if (words[j].toLowerCase().compareTo(words[i].toLowerCase()) == 0){
-                        if(!(nonUniques.contains(words[i].toLowerCase()))){
-                            nonUniques.add(words[i].toLowerCase());
+                    if (words[j].compareTo(words[i]) == 0){
+                        if(!(nonUniques.contains(words[i]))){
+                            nonUniques.add(words[i]);
                             array[j] = true;
                             count--;
                         }
